6f0a108ca4260662778a13a0893500917fe7a5a6
sxmtee <somtoo76@gmail.com>
2025-02-05 13:20:27 +0100

    Initial Commit

---
+++ StopN'Chow/StopN_ChowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopN_ChowApp.swift
+//  StopN'Chow
+//
+//  Created by mac on 05/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct StopN_ChowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
